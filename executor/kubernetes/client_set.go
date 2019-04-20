db2703017fae3ce00f805ff5842f762eef48a23f
Anton Antonov <anton.synd.antonov@gmail.com>
2019-04-20 21:13:30 +0300

    Add KubernetesClientSet

---
+++ executor/kubernetes/client_set.go
@@ -0,0 +1,121 @@
+package kubernetes
+
+import (
+	"fmt"
+
+	"github.com/palantir/stacktrace"
+
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type ClientSet struct {
+	clientSet                    *kubernetes.Clientset
+	kubeConfig                   *rest.Config
+	inCluster                    bool
+	kubernetesServiceAccountName string
+	kubernetesNamespace          string
+}
+
+func NewKubernetesClientSet(
+	kubeconfigPath,
+	kubernetesServiceAccountName,
+	kubernetesNamespace string,
+	inCluster bool,
+) (*ClientSet, error) {
+	var kubeconfig *rest.Config
+	var err error
+
+	if inCluster {
+		kubeconfig, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, stacktrace.Propagate(err, "failed to get in-cluster kubeconfig")
+		}
+
+	} else {
+		kubeconfig, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+		if err != nil {
+			return nil, stacktrace.Propagate(err, "failed to parse kubeconfig")
+		}
+	}
+
+	clientSet, err := kubernetes.NewForConfig(kubeconfig)
+	if err != nil {
+		return nil, stacktrace.Propagate(
+			err,
+			"failed to create kubernetes client set from kubeconfig",
+		)
+	}
+
+	return &ClientSet{
+		inCluster:                    inCluster,
+		clientSet:                    clientSet,
+		kubeConfig:                   kubeconfig,
+		kubernetesNamespace:          kubernetesNamespace,
+		kubernetesServiceAccountName: kubernetesServiceAccountName,
+	}, nil
+}
+
+func (c *ClientSet) GetJWT() (string, error) {
+	if c.inCluster {
+		return c.kubeConfig.BearerToken, nil
+	}
+
+	serviceAccountInfo, err := c.
+		clientSet.
+		CoreV1().
+		ServiceAccounts(c.kubernetesNamespace).
+		Get(c.kubernetesServiceAccountName, metav1.GetOptions{})
+	if err != nil {
+		return "", stacktrace.Propagate(
+			err,
+			"failed to get kubernetes dashboard service account %s from namespace %s",
+			c.kubernetesServiceAccountName,
+			c.kubernetesNamespace,
+		)
+	}
+
+	var secretName string
+
+	for _, serviceAccountSecret := range serviceAccountInfo.Secrets {
+		if strings.Contains(serviceAccountSecret.Name, "token") {
+			secretName = serviceAccountSecret.Name
+			break
+		}
+	}
+
+	if len(secretName) == 0 {
+		return "", fmt.Errorf(
+			"no kubernetes service account secret token from namespace %s for service account: %s",
+			c.kubernetesNamespace,
+			c.kubernetesServiceAccountName,
+		)
+	}
+
+	secret, err := c.clientSet.CoreV1().Secrets(c.kubernetesNamespace).Get(secretName, metav1.GetOptions{})
+	if err != nil {
+		return "", stacktrace.Propagate(
+			err, "failed to get kubernetes service account secret token data from namespace "+
+				"%s for service account: %s",
+			c.kubernetesNamespace,
+			c.kubernetesServiceAccountName,
+		)
+	}
+
+	token := secret.Data["token"]
+	if token == nil {
+		return "", stacktrace.Propagate(
+			err,
+			"no token data found in kubernetes service account secret token data "+
+				"from namespace %s for service account: %s",
+			c.kubernetesNamespace,
+			c.kubernetesServiceAccountName,
+		)
+	}
+
+	return string(token), nil
+}
--- go.mod
+++ go.mod
@@ -1,18 +1,34 @@
 module github.com/sumup-oss/go-pkgs
 
 require (
+	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
+	github.com/elazarl/goproxy v0.0.0-20170405201442-c4fc26588b6e // indirect
+	github.com/evanphx/json-patch v0.0.0-20190203023257-5858425f7550 // indirect
+	github.com/gogo/protobuf v0.0.0-20171007142547-342cbe0a0415 // indirect
+	github.com/golang/groupcache v0.0.0-20160516000752-02826c3e7903 // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf // indirect
+	github.com/google/uuid v1.0.0 // indirect
+	github.com/googleapis/gnostic v0.2.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/hashicorp/go-retryablehttp v0.5.3 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.0 // indirect
 	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/vault v1.1.2
+	github.com/imdario/mergo v0.3.7 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/json-iterator/go v0.0.0-20180701071628-ab8a2e0c74be // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/mattes/go-expand-tilde v0.0.0-20150330173918-cb884138e64c
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
+	github.com/onsi/gomega v0.0.0-20190113212917-5533ce8a0da3 // indirect
 	github.com/palantir/stacktrace v0.0.0-20161112013806-78658fd2d177
 	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
@@ -24,7 +40,16 @@ require (
 	github.com/stretchr/testify v1.3.0
 	golang.org/x/crypto v0.0.0-20190418165655-df01cb2cc480
 	golang.org/x/net v0.0.0-20190419010253-1f3472d942ba // indirect
+	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a // indirect
 	golang.org/x/sys v0.0.0-20190419153524-e8e3143a4f4a // indirect
 	golang.org/x/text v0.3.1-0.20181227161524-e6919f6577db // indirect
 	golang.org/x/time v0.0.0-20181108054448-85acf8d2951c // indirect
+	gopkg.in/inf.v0 v0.9.0 // indirect
+	k8s.io/api v0.0.0-20190313235455-40a48860b5ab // indirect
+	k8s.io/apimachinery v0.0.0-20190313205120-d7deff9243b1
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/klog v0.3.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20190228160746-b3a7cee44a30 // indirect
+	k8s.io/utils v0.0.0-20190308190857-21c4ce38f2a7 // indirect
+	sigs.k8s.io/yaml v1.1.0 // indirect
 )
