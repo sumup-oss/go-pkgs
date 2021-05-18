4c3c8a50644f44c10f5940847acffc0ec56c6eeb
Plamen Kutinchev <plamen.kutinchev@sumup.com>
2021-05-18 16:42:02 +0300

    Address comments:  - add check for b.config.BackoffResetDuration if 0  - add Retried receiver func so we can track on which retry we are.  - add comments

--- backoff/backoff.go
+++ backoff/backoff.go
@@ -4,25 +4,35 @@ import (
 	"time"
 )
 
+// RandomGenerator interface for the random generator.
+// Standard library can be used as follows: rand.NewSource(time.Now().UnixNano()).
 type RandomGenerator interface {
 	Int63n(n int64) int64
 }
 
+// Config is used for the backoff constructor if you don't want to use the default config.
 type Config struct {
-	Base                 time.Duration
-	Max                  time.Duration
-	BackOffResetDuration time.Duration
-	Jitter               Jitter
+	// Base is the duration used for the next duration calculation.
+	Base time.Duration
+	// Max is the maximum duration the backoff can return.
+	Max time.Duration
+	// BackoffResetDuration is the duration between two Next() calls that the Backoff will wait until he resets the counter.
+	BackoffResetDuration time.Duration
+	// Jitter is the Jitter used to randomize the next duration.
+	Jitter Jitter
 }
 
 var DefaultConfig = &Config{
 	Base:                 time.Second,
 	Max:                  time.Second * 30,
-	BackOffResetDuration: time.Second * 60,
+	BackoffResetDuration: time.Second * 60,
 	Jitter:               FullJitter,
 }
 
-// Backoff The backoff is calculated by min(Max, rand(0, Base*(2^retries))).
+// Backoff is used to calculate the next backoff duration using a Jitter.
+// Check here why to use Jitter - https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+// The backoff is calculated by min(Max, rand(0, Base*(2^retries))).
+// example: backoffWithDefaultConfig := backoff.New(rand.New(rand.NewSource(time.Now().UnixNano())), backoff.DefaultConfig)
 type Backoff struct {
 	config *Config
 
@@ -44,18 +54,25 @@ func New(randomGen RandomGenerator, config *Config) *Backoff {
 		config.Jitter = DefaultConfig.Jitter
 	}
 
+	if config.BackoffResetDuration == 0 {
+		config.BackoffResetDuration = DefaultConfig.BackoffResetDuration
+	}
+
 	return &Backoff{
 		config:    config,
 		randomGen: randomGen,
 	}
 }
 
+// Next returns the next duration for the retry.
 func (b *Backoff) Next() time.Duration {
 	now := time.Now()
-	elapsed := float64(now.Sub(b.tn)) / float64(time.Second)
+	elapsed := now.Sub(b.tn)
 
-	if elapsed > float64(b.config.BackOffResetDuration) {
-		b.retryCount = 0
+	if b.config.BackoffResetDuration != 0 {
+		if elapsed > b.config.BackoffResetDuration {
+			b.retryCount = 0
+		}
 	}
 
 	d := b.config.Base * (1 << b.retryCount)
@@ -68,4 +85,9 @@ func (b *Backoff) Next() time.Duration {
 	b.tn = now
 
 	return b.config.Jitter(b.randomGen, int64(d))
+}
+
+// Retried returns the number of retries
+func (b *Backoff) Retried() uint {
+	return b.retryCount
 }
--- backoff/backoff_test.go
+++ backoff/backoff_test.go
@@ -46,6 +46,8 @@ func TestNewBackOff(t *testing.T) {
 
 			duration = backoffDefault.Next()
 			assert.Equal(t, int64(401), duration.Nanoseconds())
+
+			assert.Equal(t, uint(4), backoffDefault.Retried())
 		},
 	)
 
@@ -60,12 +62,15 @@ func TestNewBackOff(t *testing.T) {
 			randomGeneratorMock.On("Int63n", getDurationForRetry(2, time.Minute)/2+1).Return(int64(300))
 			randomGeneratorMock.On("Int63n", getDurationForRetry(3, time.Minute)/2+1).Return(int64(400))
 
-			backoffEqJitter := backoff.New(randomGeneratorMock, &backoff.Config{
-				Base:                 time.Minute,
-				Max:                  time.Minute * 10,
-				BackOffResetDuration: time.Minute * 20,
-				Jitter:               backoff.EqualJitter,
-			})
+			backoffEqJitter := backoff.New(
+				randomGeneratorMock,
+				&backoff.Config{
+					Base:                 time.Minute,
+					Max:                  time.Minute * 10,
+					BackoffResetDuration: time.Minute * 20,
+					Jitter:               backoff.EqualJitter,
+				},
+			)
 
 			duration := backoffEqJitter.Next()
 			assert.Equal(t, int64(30000000101), duration.Nanoseconds())
@@ -78,6 +83,47 @@ func TestNewBackOff(t *testing.T) {
 
 			duration = backoffEqJitter.Next()
 			assert.Equal(t, int64(240000000401), duration.Nanoseconds())
+
+			assert.Equal(t, uint(4), backoffEqJitter.Retried())
+		},
+	)
+
+	t.Run(
+		"check backoff reset duration implementation",
+		func(t *testing.T) {
+			t.Parallel()
+			randomGeneratorMock := &RandomGeneratorMock{}
+			randomGeneratorMock.On("Int63n", getDurationForRetry(0, time.Millisecond)).Return(int64(100))
+			randomGeneratorMock.On("Int63n", getDurationForRetry(1, time.Millisecond)).Return(int64(200))
+			randomGeneratorMock.On("Int63n", getDurationForRetry(2, time.Millisecond)).Return(int64(300))
+
+			backoffDefault := backoff.New(
+				randomGeneratorMock,
+				&backoff.Config{
+					Base:                 time.Millisecond,
+					Max:                  time.Millisecond * 10,
+					BackoffResetDuration: time.Millisecond * 20,
+					Jitter:               backoff.FullJitter,
+				},
+			)
+			duration := backoffDefault.Next()
+
+			assert.Equal(t, int64(101), duration.Nanoseconds())
+
+			duration = backoffDefault.Next()
+			assert.Equal(t, int64(201), duration.Nanoseconds())
+
+			duration = backoffDefault.Next()
+			assert.Equal(t, int64(301), duration.Nanoseconds())
+
+			assert.Equal(t, uint(3), backoffDefault.Retried())
+
+			time.Sleep(20 * time.Millisecond)
+
+			duration = backoffDefault.Next()
+			assert.Equal(t, int64(101), duration.Nanoseconds())
+
+			assert.Equal(t, uint(1), backoffDefault.Retried())
 		},
 	)
 }
