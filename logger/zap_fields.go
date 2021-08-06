76eb17301bd76b5cdbd4376431c9dcbaaed7a169
Daniel Lyubomirov <dennislt@gmail.com>
2021-08-06 16:24:03 +0300

    Add logger http request, response dump fields factories.

--- logger/zap_fields.go
+++ logger/zap_fields.go
@@ -15,6 +15,10 @@
 package logger
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -37,5 +41,93 @@ func ErrorField(err error) zap.Field {
 		Key:       "error",
 		Type:      zapcore.ErrorType,
 		Interface: err,
+	}
+}
+
+type httpRequestDumpField struct {
+	req      *http.Request
+	dumpBody bool
+}
+
+func (r *httpRequestDumpField) String() string {
+	dump, err := httputil.DumpRequest(r.req, r.dumpBody)
+	if err != nil {
+		return fmt.Sprintf("request dump failed, err: %v", err)
+	}
+
+	return string(dump)
+}
+
+// HTTPRequestDump creates a zap.Field that can dump http request lazily.
+//
+// It is typically used with logger Debug calls.
+// NOTE: The http request body is not dumped. For that use HTTRequestDumpBody.
+func HTTPRequestDump(key string, req *http.Request) zap.Field {
+	return zap.Field{
+		Key:  key,
+		Type: zapcore.StringerType,
+		Interface: &httpRequestDumpField{
+			req:      req,
+			dumpBody: false,
+		},
+	}
+}
+
+// HTTPRequestDumpBody creates a zap.Field that can dump http request along with its body lazily.
+//
+// It is typically used with logger Debug calls.
+// NOTE: If you want to dump the request path and headers only, use HTTPRequestDump.
+func HTTPRequestDumpBody(key string, req *http.Request) zap.Field {
+	return zap.Field{
+		Key:  key,
+		Type: zapcore.StringerType,
+		Interface: &httpRequestDumpField{
+			req:      req,
+			dumpBody: true,
+		},
+	}
+}
+
+type httpResponseDumpField struct {
+	req      *http.Response
+	dumpBody bool
+}
+
+func (r *httpResponseDumpField) String() string {
+	dump, err := httputil.DumpResponse(r.req, r.dumpBody)
+	if err != nil {
+		return fmt.Sprintf("response dump failed, err: %v", err)
+	}
+
+	return string(dump)
+}
+
+// HTTPResponseDump creates a zap.Field that can dump http response lazily.
+//
+// It is typically used with logger Debug calls.
+// NOTE: The http response body is not dumped. For that use HTTResponseDumpBody.
+func HTTPResponseDump(key string, req *http.Response) zap.Field {
+	return zap.Field{
+		Key:  key,
+		Type: zapcore.StringerType,
+		Interface: &httpResponseDumpField{
+			req:      req,
+			dumpBody: false,
+		},
+	}
+}
+
+// HTTPResponseDumpBody creates a zap.Field that can dump http request along with its body lazily.
+//
+// It is typically used with logger Debug calls.
+// NOTE: If you want to dump the response path and headers only, use HTTPResponseDump.
+func HTTPResponseDumpBody(key string, req *http.Response) zap.Field {
+	return zap.Field{
+		Key:  key,
+		Type: zapcore.StringerType,
+		Interface: &httpResponseDumpField{
+			req:      req,
+			dumpBody: true,
+		},
 	}
 }
